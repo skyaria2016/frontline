a876a7023d5cc1983b426f162b12ae2a5fa6de78
skyaria2016 <skyaria2016@gmail.com>
2017-07-16 22:16:14 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+﻿var myImage = document.getElementById("mainImage");
+var imageArray = ["main.jpg", "5.jpg", "main.jpg", "5.jpg"];
+var imageIndex = 0;
+
+function changeImage() {
+    myImage.setAttribute("src", imageArray[imageIndex]);
+    imageIndex++;
+    if (imageIndex >= imageArray.length) {
+        imageIndex = 0;
+    }
+}
+setInterval(changeImage, 3000);
